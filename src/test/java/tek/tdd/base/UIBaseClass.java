d4291f7fa0176963ac007b1453a481c65f378c1c
darya <varenikdarya@gmail.com>
2024-08-15 14:33:14 -0400

    using POM in test

---
+++ src/test/java/tek/tdd/base/UIBaseClass.java
@@ -0,0 +1,27 @@
+package tek.tdd.base;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import tek.tdd.page.HomePage;
+import tek.tdd.utility.SeleniumUtility;
+
+
+    public class UIBaseClass extends SeleniumUtility {
+        private static final Logger LOGGER = LogManager.getLogger(UIBaseClass.class);
+        public HomePage homePage;
+        @BeforeMethod
+        public void setupTests() {
+            LOGGER.info("Setup Test and opening browser");
+            setupBrowser();
+            homePage = new HomePage();
+        }
+
+        @AfterMethod
+        public void testCleanup() {
+            LOGGER.info("running after each test and quite browser");
+            quitBrowser();
+        }
+
+    }
---
+++ src/test/java/tek/tdd/page/SignInPage.java
@@ -0,0 +1,24 @@
+package tek.tdd.page;
+
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import tek.tdd.utility.SeleniumUtility;
+
+public class SignInPage extends SeleniumUtility {
+
+        public SignInPage() {
+            PageFactory.initElements(getDriver(), this);
+        }
+
+        @FindBy(name = "email")
+        public WebElement emailInput;
+
+        @FindBy(name = "password")
+        public WebElement passwordInput;
+
+        @FindBy(id = "loginBtn")
+        public WebElement loginButton;
+    }
+
